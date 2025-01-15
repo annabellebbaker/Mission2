271e05cc3bab8da7975321bc415f11f20d647fd8
annabellebbaker <annabellebaker0922@gmail.com>
2025-01-15 13:06:58 -0700

    Add project files.

---
+++ DiceSimulation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSimulation;
+
+internal class DiceSimulation
+{
+    // this class will be calculating the results of the dice rolls
+    private readonly Random random = new Random();
+
+    // creating an instance of a random class
+    public int[] DiceResults(int input) // want it to be public so it can be referenced later in the Program.cs
+    { 
+    int[] results = new int[13]; // creating an instance for the empty array to store numbers (how many of each combination is rolled)
+
+    for (int iCount = 0; iCount < input; iCount++) // incrementor for loop., incrementing through each roll
+        {
+            int die1 = random.Next(1, 7); // limits of the random number generated 
+            int die2 = random.Next(1, 7);
+            int sum = die1 + die2; // don't forget to call what variable type it is
+            results[sum]++; // coordinates in the array - put it in its spot and then putting a "tally" esssential
+        }
+    return results; // see the results of the dice rolls totals
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Annabelle Baker, 2-10
+
+// Write a .NET console application using C# that simulates the rolling of two 6-sided dice (NOTE:
+// NOT a one 12-sided die.)
+// Use an array to keep track of the number of times that each combination is thrown.
+// Allow the user to choose how many times the “dice” will be thrown
+
+// ask the user for how many times to roll the dice
+// write down how many times they need to roll
+// for the number of times the user said
+//      roll die 1 and collect that result
+//      roll die 2 and collect that result
+//      add together and store result
+
+// for each number possible to be rolled
+//      print the number
+//      calculate the percentage of the times it was rolled
+//      print an asterisk for each percent it was rolled
+// the random number should be a combination of the two die
+// use an array to store it (how do I want to store it?) 
+// use a for loop to print out the number of ***
+   
+using System;
+
+namespace DiceSimulation; // calling the project to get the contents of the other file
+internal class Program // container
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        int input = int.Parse(Console.ReadLine()); // reading this line of whatever the user is inputting and converting to integer
+
+        DiceSimulation simulation = new DiceSimulation(); // creation of this instance
+
+        int[] displayResults = simulation.DiceResults(input); // pulling from the method of the DiceSimulation class
+
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS"); // printing all of the sample session lines
+        
+        Console.WriteLine("Each '*' represents 1% of the total number of rolls. ");
+
+        Console.WriteLine("Total number of rolls = " + input); // total number that a user inputs
+
+        PrintHistogram(displayResults, input);
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Simulator out!");
+    }
+
+    private static void PrintHistogram(int[] results, int totalRolls)
+    {
+        for (int iCount = 2; iCount <= 12; iCount++)
+        {
+            int percentage = results[iCount] * 100 / totalRolls;
+
+            string asterisk = new string('*', percentage); // creating a variable called asterisk and the percentage
+
+            Console.WriteLine($"{iCount}: {asterisk}"); // I chose to do f-string here because of the 2+ variables
+        }
+    }
+}
